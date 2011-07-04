6348b3095aa7e0d3e9d246d40c5b03fc073a7beb
Matt Biddulph <mb@hackdiary.com>
2011-07-04 22:33:51 +0200

    yow, XCode put "Copyright Nokia" all the way through my files. I wonder where it got that from.

--- secondscreen/secondscreenViewController.m
+++ secondscreen/secondscreenViewController.m
@@ -3,7 +3,7 @@
 //  secondscreen
 //
 //  Created by Matt Biddulph on 04/07/2011.
-//  Copyright 2011 Nokia. All rights reserved.
+//  Copyright 2011 Hackdiary Ltd. All rights reserved.
 //
 
 #import "secondscreenViewController.h"
--- secondscreenTests/secondscreenTests.m
+++ secondscreenTests/secondscreenTests.m
@@ -3,7 +3,7 @@
 //  secondscreenTests
 //
 //  Created by Matt Biddulph on 04/07/2011.
-//  Copyright 2011 Nokia. All rights reserved.
+//  Copyright 2011 Hackdiary Ltd. All rights reserved.
 //
 
 #import "secondscreenTests.h"
